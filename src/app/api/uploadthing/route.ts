b71df154053bc1ecaaf9e23eeaff8e87d4524976
seekrr03 <133124629+seekrr03@users.noreply.github.com>
2026-01-29 12:18:19 +0000

    uploadthing

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,7 +1,8 @@
+// src/app/api/uploadthing/route.ts
 import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 
-// Export routes for Next.js App Router
+// Export the GET and POST handlers
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
 });
